cc1ee0f61e37d792b246a15df819f24f3999bf42
hyese95 <hyese3085@gmail.com>
2025-02-10 16:14:38 +0900

    sql

--- bithive.sql
+++ bithive.sql
@@ -18,6 +18,8 @@ CREATE USER 'bithive_dev'@'localhost' IDENTIFIED BY 'bithive';
 CREATE USER 'bithive_user'@'localhost' IDENTIFIED BY 'bithive';
 
 GRANT 'dev_role' TO 'bithive_dev'@'localhost';
+SET DEFAULT ROLE 'dev_role' TO 'bithive_dev'@'localhost';
+FLUSH PRIVILEGES;
 GRANT 'user_role' TO 'bithive_user'@'localhost';
 
 USE bithive;
@@ -26,9 +28,9 @@ SELECT 'CREATING DATABASE STRUCTURE' as 'INFO';
 
 
 CREATE TABLE users (
-                       user_no VARCHAR(10) PRIMARY KEY,  -- u001 형식
+                       user_no VARCHAR(10) PRIMARY KEY,
                        user_id VARCHAR(50) NOT NULL UNIQUE,
-                       password VARCHAR(255) NOT NULL,    -- 암호화된 비밀번호 저장을 고려해 넉넉한 길이
+                       password VARCHAR(255) NOT NULL,
                        nickname VARCHAR(50) NOT NULL,
                        email VARCHAR(100) NOT NULL UNIQUE,
                        phone VARCHAR(20) NOT NULL,
@@ -36,15 +38,14 @@ CREATE TABLE users (
                        marketing_agreements BOOLEAN NOT NULL DEFAULT false,
                        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                        profile_img_url VARCHAR(255),
-                       nationality ENUM('KOREAN', 'JAPAN') NOT NULL,  -- 실제 사용할 값들로 수정 필요
+                       nationality ENUM('KOREAN', 'JAPAN') NOT NULL,
                        gender ENUM('MALE', 'FEMALE') NOT NULL,
-                       theme ENUM('LIGHT', 'DARK') NOT NULL DEFAULT 'LIGHT',  -- 실제 사용할 테마 값으로 수정 필요
+                       theme ENUM('LIGHT', 'DARK') NOT NULL DEFAULT 'LIGHT',
                        birth_date TIMESTAMP,
                        name VARCHAR(50) NOT NULL,
                        self_introduction TEXT,
                        role ENUM('USER', 'MANGER') NOT NULL DEFAULT 'USER'
 );
-
 
 
 
