9f8a464404e95de92d1e8df988598d90b3842352
hyese95 <hyese3085@gmail.com>
2025-02-26 15:55:15 +0900

    rr

---
+++ src/test/java/com/codehive/codehive/CodehiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehive.codehive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodehiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
