9c241216c644648b8a62867e56baa5a672c2c5bb
SEUNGYEOB IN <seungyeob@SEUNGYEOBui-MacBookPro.local>
2025-03-06 03:26:45 +0900

    네비게이션 bar 및 컨트롤러 초기설정

---
+++ src/main/java/com/example/codehive/controller/TradeController.java
@@ -0,0 +1,15 @@
+package com.example.codehive.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/trade")
+public class TradeController {
+
+    @GetMapping("main.do")
+    public String main() {
+        return "trade/main";
+    }
+}
