909ca2084fcedd622cc654c8cc09cdab88eafa9d
realsydrid <realsydrid@gmail.com>
2025-03-14 15:08:03 +0900

    최근검색어 쿠키(비동처리출력)로 구현

--- src/main/java/com/example/codehive/controller/CommunityController.java
+++ src/main/java/com/example/codehive/controller/CommunityController.java
@@ -4,21 +4,28 @@ import com.example.codehive.dto.PostDto;
 import com.example.codehive.entity.Post;
 import com.example.codehive.entity.User;
 import com.example.codehive.service.PostService;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import com.example.codehive.service.UserService;
 import lombok.AllArgsConstructor;
 import com.example.codehive.repository.PostRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+@Slf4j
 @Controller
 @RequestMapping("/community")
 @AllArgsConstructor
@@ -33,9 +40,10 @@ public class CommunityController {
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size, Pageable pageable) {
         pageable = PageRequest.of(page, size);
-        Page<Post> postPage=postService.readAllByCategory(pageable,"free");
+        Page<Post> postPage = postService.readAllByCategory(pageable, "free");
         return postPage.map(PostDto::new);
     }
+
     @GetMapping("/free_post.do")
     public String freePost(Model model,
                            @PageableDefault(size = 10) Pageable pageable,
@@ -56,13 +64,14 @@ public class CommunityController {
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size, Pageable pageable) {
         pageable = PageRequest.of(page, size);
-        Page<Post> postPage=postService.readAllByCategory(pageable,"pnl");
+        Page<Post> postPage = postService.readAllByCategory(pageable, "pnl");
         return postPage.map(PostDto::new);
     }
+
     @GetMapping("/pnl_post.do")
     public String pnlPost(Model model,
-                           @PageableDefault(size = 10) Pageable pageable,
-                           @RequestParam(defaultValue = "0") int page) {// 기본 페이지 값 설정
+                          @PageableDefault(size = 10) Pageable pageable,
+                          @RequestParam(defaultValue = "0") int page) {// 기본 페이지 값 설정
         pageable = PageRequest.of(page, 10);
         Page<Post> pnlPostPage = postService.readAllByCategory(pageable, "pnl");
         List<User> user = userService.findAll();
@@ -74,17 +83,24 @@ public class CommunityController {
     @GetMapping("/postDetail.do")
     public String detail(Model model,
                          @RequestParam int postNo
-                         ) {
+    ) {
         System.out.println("받은 아이디는" + postNo);
-        Post post=postService.getPostByPostId(postNo);
+        Post post = postService.getPostByPostId(postNo);
         model.addAttribute("post", post);
         return "community/postDetail";
     }
 
     @GetMapping("search.do")
     public String search(Model model,
-                         @RequestParam(defaultValue = "all") String category
+                         @RequestParam(defaultValue = "all") String category, @CookieValue(required = false) String recentKeywords
     ) {
+        if (recentKeywords != null) {
+            Set<String> keywordSet = new HashSet<>(Arrays.asList(recentKeywords.split("-")));
+            model.addAttribute("keywordSet", keywordSet);
+            model.addAttribute("category", category);
+
+        }
+
         return "community/search";
     }
 
@@ -95,6 +111,31 @@ public class CommunityController {
                                 HttpServletRequest request,
                                 HttpServletResponse response
     ) {
+        //쿠키추가
+        Cookie[] cookies = request.getCookies();
+        Cookie keywordCookie = null;
+        Set<String> keywordSet = new HashSet<>();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("recentKeywords")) {
+                    keywordCookie = cookie;
+                    String[] keywordArray = cookie.getValue().split("-");
+                    keywordSet.addAll(Arrays.asList(keywordArray));
+                    break;
+                }
+            }
+        }
+        keywordSet.add(keyword);
+        if (keywordCookie == null) {
+            keywordCookie = new Cookie("recentKeywords", keyword);
+        } else {
+            keywordCookie.setValue(String.join("-", keywordSet));
+        }
+        keywordCookie.setMaxAge(60 * 60 * 24 * 7);
+        keywordCookie.setPath("/community");
+        response.addCookie(keywordCookie);
+
+        //검색로직
         Pageable pageable;
         if (category.equals("all")) { //통합검색
             pageable = PageRequest.of(0, 2);
@@ -113,11 +154,67 @@ public class CommunityController {
         }
         model.addAttribute("category", category);
         model.addAttribute("keyword", keyword);
-
-
-
         return "community/search_result";
     }
+
+    @GetMapping("search/cookieDelete.do")
+    public ResponseEntity<Void> cookieDelete(
+            @RequestParam String keyword,
+            HttpServletResponse response,
+            HttpServletRequest request
+    ) {
+        try {
+            System.out.println(keyword);
+            Cookie[] cookies = request.getCookies();
+            Cookie keywordCookie = null;
+            Set<String> keywordSet = new HashSet<>();
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("recentKeywords")) {
+                    keywordCookie = cookie;
+                    String[] keywordArray = cookie.getValue().split("-");
+                    keywordSet.addAll(Arrays.asList(keywordArray));
+                    keywordSet.remove(keyword);
+                    break;
+                }
+            }
+            if (!keywordSet.isEmpty()) {
+                keywordCookie.setValue(String.join("-", keywordSet));
+                keywordCookie.setMaxAge(60 * 60 * 24 * 7);
+                keywordCookie.setPath("/community");
+                response.addCookie(keywordCookie);
+            } else {
+                keywordCookie.setValue("");
+                keywordCookie.setPath("/community");
+                keywordCookie.setMaxAge(0);
+                response.addCookie(keywordCookie);
+            }
+            return ResponseEntity.status(200).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).build();
+        }
+
+    }
+    @GetMapping("/search/deleteAllKeywordCookie")
+    public ResponseEntity<Void> deleteAllKeywordsCookie(HttpServletResponse response,
+                                                        HttpServletRequest request
+    ) {
+        Cookie[] cookies = request.getCookies();
+        Cookie keywordCookie = null;
+        Set<String> keywordSet = new HashSet<>();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("recentKeywords")) {
+                keywordCookie = cookie;
+            }
+        }
+        keywordCookie.setValue("");
+        keywordCookie.setPath("/community");
+        keywordCookie.setMaxAge(0);
+        response.addCookie(keywordCookie);
+        System.out.println("전체삭제요");
+        return ResponseEntity.status(200).build();
+    }
+
 
     @GetMapping("api/search")
     @ResponseBody
