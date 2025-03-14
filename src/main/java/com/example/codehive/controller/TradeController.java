a54869125ac6766532e1a918f8a3280d8049bfc5
hyese95 <hyese3085@gmail.com>
2025-03-14 16:31:17 +0900

    매수매도페이지

--- src/main/java/com/example/codehive/controller/TradeController.java
+++ src/main/java/com/example/codehive/controller/TradeController.java
@@ -5,11 +5,16 @@ import com.example.codehive.entity.FavoriteMarket;
 import com.example.codehive.service.CoinTransactionService;
 import com.example.codehive.service.FavoriteCoinMarketService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/trade")
@@ -35,7 +40,7 @@ public class TradeController {
     }
 
     @GetMapping("buy.do")
-    public String coinOrder() {
+    public String coinOrder(Model model) {
         return "trade/buy";
     }
 
@@ -53,4 +58,24 @@ public class TradeController {
         model.addAttribute("myAssetList", myAssetList);
         return "trade/holding_coin";
     }
-}
+
+    @GetMapping("/orderbook/{market}")
+    @ResponseBody
+    public Map<String, Object> getOrderBookData(@PathVariable String market) {
+        String orderbookUrl = "https://api.upbit.com/v1/orderbook?markets=" + market;
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<List<Map<String, Object>>> response = restTemplate.exchange(
+                    orderbookUrl, HttpMethod.GET, null,
+                    new ParameterizedTypeReference<List<Map<String, Object>>>() {}
+            );
+            if (response.getBody() != null && !response.getBody().isEmpty()) {
+                return response.getBody().get(0);
+            } else {
+                throw new RuntimeException("No data found for market: " + market);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to fetch orderbook data for market: " + market);
+        }
+    }}
