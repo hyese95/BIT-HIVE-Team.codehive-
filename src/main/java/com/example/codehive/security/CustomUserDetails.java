7b99574f09d42fcdcbfc386209ec29860a431f56
hyese95 <hyese3085@gmail.com>
2025-05-01 15:04:32 +0900

    커스텀디테일수정

--- src/main/java/com/example/codehive/security/CustomUserDetails.java
+++ src/main/java/com/example/codehive/security/CustomUserDetails.java
@@ -1,6 +1,8 @@
 package com.example.codehive.security;
 
 import com.example.codehive.entity.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -10,27 +12,24 @@ import java.util.Collection;
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
 public class CustomUserDetails implements UserDetails {
     private final User user;
-
-    public CustomUserDetails(User user) {
-        this.user = user;
-    }
 
     // 권한 부여
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
     }
-
+    @JsonIgnore
     @Override
     public String getPassword() {
-        return user.getPassword(); // DB 비번
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return user.getUserId(); // 로그인 ID
+        return user.getUserId();
     }
 
     @Override public boolean isAccountNonExpired() { return true; }
@@ -38,8 +37,4 @@ public class CustomUserDetails implements UserDetails {
     @Override public boolean isCredentialsNonExpired() { return true; }
     @Override public boolean isEnabled() { return true; }
 
-    public String getNickname() { return user.getNickname(); }
-    public String getName() { return user.getName(); }
-    public String getRole() { return user.getRole().name(); }
-    public Integer getUserNo() { return user.getId(); }
 }
