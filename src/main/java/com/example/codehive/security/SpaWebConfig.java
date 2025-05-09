0fcac5f0020d762d0f887729f676f5b38ed122f6
hyese95 <hyese3085@gmail.com>
2025-05-09 10:19:31 +0900

    찐찐찐찐배포

--- src/main/java/com/example/codehive/security/SpaWebConfig.java
+++ src/main/java/com/example/codehive/security/SpaWebConfig.java
@@ -1,6 +1,7 @@
 package com.example.codehive.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,5 +14,14 @@ public class SpaWebConfig implements WebMvcConfigurer {
                 .setViewName("forward:/index.html");
         registry.addViewController("//{spring:[^.]*}")
                 .setViewName("forward:/index.html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // static 내부 파일(js, css, 이미지 등)은 그대로 내보낸다
+        registry
+                .addResourceHandler("/**/*.js", "/**/*.css", "/**/*.html",
+                        "/**/*.png","/**/*.svg","/**/*.ico")
+                .addResourceLocations("classpath:/static/");
     }
 }
