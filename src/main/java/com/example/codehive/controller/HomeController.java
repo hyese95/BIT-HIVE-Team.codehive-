8842ebd91e507f54ca001f7554b4877c274a4893
hyese95 <hyese3085@gmail.com>
2025-05-09 11:01:24 +0900

    배포

--- src/main/java/com/example/codehive/controller/HomeController.java
+++ src/main/java/com/example/codehive/controller/HomeController.java
@@ -3,19 +3,19 @@ package com.example.codehive.controller;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
 
 
-    @GetMapping("/")
-//@GetMapping({
-//        "/",                             // ①
-//        "/{path:[^\\.]+}",               // ②  (점이 없는 1단)
-//        "/**/{path:[^\\.]+}"             // ③  (점이 없는 다단)
-//})
+//    @GetMapping("/")
+@GetMapping({
+        "/",                             // ①
+        "/{path:[^\\.]+}",               // ②  (점이 없는 1단)
+        "/**/{path:[^\\.]+}"             // ③  (점이 없는 다단)
+})
     public String home() {
         return "forward:index.html";
     }
-
 }
