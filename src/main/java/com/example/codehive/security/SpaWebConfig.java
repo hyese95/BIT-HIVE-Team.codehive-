df4b732c869924a2e8b1e8fc66bd97f9e585940e
hyese95 <hyese3085@gmail.com>
2025-05-09 10:48:17 +0900

    spa 재배포2

--- src/main/java/com/example/codehive/security/SpaWebConfig.java
+++ src/main/java/com/example/codehive/security/SpaWebConfig.java
@@ -7,21 +7,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SpaWebConfig implements WebMvcConfigurer {
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        // /api/ 가 아닌 모든 경로(React 라우트)를 index.html 로 매핑
-        registry.addViewController("/{spring:[^.]*}")
-                .setViewName("forward:/index.html");
-        registry.addViewController("//{spring:[^.]*}")
-                .setViewName("forward:/index.html");
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // static 내부 파일(js, css, 이미지 등)은 그대로 내보낸다
-        registry
-                .addResourceHandler("/**/*.js", "/**/*.css", "/**/*.html",
-                        "/**/*.png","/**/*.svg","/**/*.ico")
-                .addResourceLocations("classpath:/static/");
-    }
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        // /api/ 가 아닌 모든 경로(React 라우트)를 index.html 로 매핑
+//        registry.addViewController("/{spring:[^.]*}")
+//                .setViewName("forward:/index.html");
+//        registry.addViewController("//{spring:[^.]*}")
+//                .setViewName("forward:/index.html");
+//    }
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        // static 내부 파일(js, css, 이미지 등)은 그대로 내보낸다
+//        registry
+//                .addResourceHandler("/**/*.js", "/**/*.css", "/**/*.html",
+//                        "/**/*.png","/**/*.svg","/**/*.ico")
+//                .addResourceLocations("classpath:/static/");
+//    }
 }
