4c885ef9432e104aa350c8f93b4a38505c8eaa7d
Arkite124 <ghkddudwnd@naver.com>
2025-05-11 00:44:10 +0900

    control Dto Renewal

---
+++ src/main/java/com/example/codehive/dto/CommentAndUserLikeDto.java
@@ -0,0 +1,38 @@
+package com.example.codehive.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public class CommentAndUserLikeDto {
+    private Integer id;
+    private Integer parentNo;
+    private Integer postNo;
+    private String commentCont;
+    private String userProfileImg;
+    @JsonFormat(pattern = "yyyy-MM-dd a hh시 MM분", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime CommentCreatedAt;
+    private String category;
+    private String userNickname;
+    private Integer userNo;
+    private int likeCount;
+    private int dislikeCount;
+    private Integer replyCount;
+    private Boolean userLikeType;
+
+    public CommentAndUserLikeDto(CommentDto commentDto,Boolean userLikeType) {
+        this.id=commentDto.getId();
+        this.parentNo=commentDto.getParentNo();
+        this.postNo=commentDto.getPostNo();
+        this.commentCont=commentDto.getCommentCont();
+        this.userProfileImg=commentDto.getUserProfileImg();
+        this.CommentCreatedAt=commentDto.getCommentCreatedAt();
+        this.category=commentDto.getCategory();
+        this.userNickname=commentDto.getUserNickname();
+        this.userNo=commentDto.getUserNo();
+        this.likeCount=commentDto.getLikeCount();
+        this.dislikeCount=commentDto.getDislikeCount();
+        this.replyCount=commentDto.getReplyCount();
+        this.userLikeType=userLikeType;
+    }
+}
